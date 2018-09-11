17b7cf91a2a42211c2a34c7a938b7c2d862915f2
cyrsis <em3888@gmail.com>
2018-09-11 13:53:28 +0800

    Add Router and browser history

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, browserHistory } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={browserHistory}/>
+    , document.getElementById('root'));
 registerServiceWorker();
